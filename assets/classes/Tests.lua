d6c0467e4050ea3fcd606388ce190799a6f112b9
Louis Perrochon <perrochon@gmail.com>
2020-05-07 20:58:59 -0700

    Put tests behind a flag

--- assets/classes/Tests.lua
+++ assets/classes/Tests.lua
@@ -1,14 +1,19 @@
-DEBUG("Tests are running")
 
--- Can we read all level files successfully?
-for i = 1, 6 do
-	local mapFile = string.format("level%02d", i)
-	mapData=MapData.new(mapFile)
-	mapData:layerFromMap(1)
-	mapData:layerFromMap(2)
-	monsters = Monsters.new(mapData)
-	monsters:Test1()
-	monsters:Test2()
+RUN_TESTS @ false
+
+if RUN_TESTS then
+	DEBUG("Tests are running")
+	-- Can we read all level files successfully?
+	for i = 1, 6 do
+		local mapFile = string.format("level%02d", i)
+		mapData=MapData.new(mapFile)
+		mapData:layerFromMap(1)
+		mapData:layerFromMap(2)
+		monsters = Monsters.new(mapData)
+		monsters:Test1()
+		monsters:Test2()
+	end
+	DEBUG("Tests completed")
+else
+	INFO("Not running tests")
 end
-
-DEBUG("Tests completed")
